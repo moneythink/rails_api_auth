a8454d32d7bfdfb33f75908a30dae5f68fe9397b
Colin Young <me@colinyoung.com>
2016-08-26 14:15:47 -0500

    Clarifying comments

--- db/migrate/20400904110441_remove_unique_index_from_identification.rb
+++ db/migrate/20400904110441_remove_unique_index_from_identification.rb
@@ -1,7 +1,10 @@
 class RemoveUniqueIndexFromIdentification < ActiveRecord::Migration
 
   def change
+    # remove previous, unique index
     remove_index :logins, :identification
+
+    # add new index without uniqueness
     add_index :logins, :identification
   end
 

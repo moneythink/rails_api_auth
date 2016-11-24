b20d57b55f18460ccf573cf1c7f026ef29be1660
Colin Young <me@colinyoung.com>
2016-11-23 20:24:53 -0600

    Reversible migration for add_foreign_key

--- db/migrate/20161122110439_add_foreign_key_constraints_to_login.rb
+++ db/migrate/20161122110439_add_foreign_key_constraints_to_login.rb
@@ -1,9 +1,8 @@
 class AddForeignKeyConstraintsToLogin < ActiveRecord::Migration
 
   def change
+    return if !foreign_keys(:logins).select {|fk| fk.to_table == 'users' }.empty?
     add_foreign_key :logins, :users
-  rescue
-    true
   end
 
 end
